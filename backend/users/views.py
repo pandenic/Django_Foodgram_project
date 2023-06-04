91b19c77785d91e8841a75cc9c7077430b4a32ac
Denis <panasenkodf@gmail.com>
2023-06-04 10:24:10 +0400

    Add me endpoint to UserViewSet.

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from users.serializers import UserSerializer
 from users.pagination import UserPagination
@@ -14,3 +15,10 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     pagination_class = UserPagination
+
+    def me(self, request):
+        """Process 'users/me' endpoint."""
+        serializer = UserSerializer(request.user)
+        if serializer.data:
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
