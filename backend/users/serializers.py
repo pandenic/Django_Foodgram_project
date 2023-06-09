d51793807036d33d88e6d87221382fe756910fe0
Denis <panasenkodf@gmail.com>
2023-06-09 18:08:27 +0400

    Making token and set-password process.

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,6 @@
 """Describe custom serializers for the users app."""
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import check_password
 from rest_framework import serializers
 from rest_framework.validators import UniqueValidator
 
@@ -8,7 +9,7 @@ from recipes.models import Follow
 User = get_user_model()
 
 
-class UserSerializer(serializers.ModelSerializer):
+class GetUserSerializer(serializers.ModelSerializer):
     """Serialize User model on get request."""
 
     is_subscribed = serializers.SerializerMethodField()
@@ -23,21 +24,55 @@ class UserSerializer(serializers.ModelSerializer):
 
         model = User
         fields = (
+            'email',
+            'id',
             'username',
-            'password',
             'first_name',
             'last_name',
-            'email',
             'is_subscribed',
         )
 
     def get_is_subscribed(self, obj):
         """Check if current user follow this user."""
-        return Follow.objects.filter(
-            follower=self.context['request'].user.id,
-        ).filter(
-            following=obj.id,
-        ).exists()
+        request = self.context.get('request')
+        if request:
+            return Follow.objects.filter(
+                follower=request.user.id,
+            ).filter(
+                following=obj.id,
+            ).exists()
+        return False
+
+
+class PostUserSerializer(serializers.ModelSerializer):
+    """Serialize User model on post request."""
+
+    email = serializers.EmailField(
+        required=True,
+        validators=(UniqueValidator(queryset=User.objects.all()),),
+        max_length=254,
+    )
+
+    class Meta:
+        """Describe settings for PostUserSerializer."""
+
+        model = User
+        fields = (
+            'email',
+            'username',
+            'first_name',
+            'last_name',
+            'password',
+        )
+
+    def to_representation(self, data):
+        return {
+            'email': data.email,
+            'id': data.id,
+            'username': data.username,
+            'first_name': data.first_name,
+            'last_name': data.last_name,
+        }
 
     def validate(self, attrs):
         """Validate serialized data of UserSerializer."""
@@ -45,18 +80,6 @@ class UserSerializer(serializers.ModelSerializer):
             if not value:
                 raise serializers.ValidationError({attr: 'Обязательное поле.'})
         return attrs
-
-    def to_representation(self, data):
-        representation = {
-            'email': data.email,
-            'id': data.id,
-            'username': data.username,
-            'first_name': data.first_name,
-            'last_name': data.last_name,
-        }
-        if self.context['request'].method == 'POST':
-            representation['is_subscribed'] = self.is_subscribed
-        return representation
 
 
 class SetPasswordSerializer(serializers.Serializer):
@@ -68,7 +91,7 @@ class SetPasswordSerializer(serializers.Serializer):
     def validate_current_password(self, value):
         """Check if current password is correct."""
         user = self.context['request'].user
-        if not user.check_password(self.current_password):
+        if not check_password(value, user.password):
             raise serializers.ValidationError({'current_password': 'Пароль неверный'})
         return value
 
--- backend/users/views.py
+++ backend/users/views.py
@@ -4,12 +4,13 @@ from django.contrib.auth.hashers import make_password, check_password
 from django.shortcuts import get_object_or_404
 
 from rest_framework import status, permissions, serializers
-from rest_framework.decorators import action, api_view
+from rest_framework.decorators import action, api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.mixins import ListCreateViewSet
-from users.serializers import UserSerializer, SetPasswordSerializer, GetTokenSerializer
+from users.serializers import GetUserSerializer, PostUserSerializer, SetPasswordSerializer, GetTokenSerializer
 from users.pagination import UserPagination
 
 User = get_user_model()
@@ -19,7 +20,7 @@ class UserViewSet(ListCreateViewSet):
     """Perform CRUD operations for User model."""
 
     queryset = User.objects.all()
-    serializer_class = UserSerializer
+    serializer_class = GetUserSerializer
     pagination_class = UserPagination
 
     @action(
@@ -29,9 +30,8 @@ class UserViewSet(ListCreateViewSet):
     )
     def me(self, request):
         """Process './me' endpoint."""
-        serializer = UserSerializer(request.user)
-        serializer.is_valid(raise_exception=True)
-        return Response(serializer.validated_data, status=status.HTTP_204_NO_CONTENT)
+        serializer = GetUserSerializer(request.user)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
 
     @action(
         ('post',),
@@ -40,7 +40,11 @@ class UserViewSet(ListCreateViewSet):
     )
     def set_password(self, request):
         """Process './set_password' endpoint."""
-        serializer = SetPasswordSerializer(data=request.data)
+        serializer = SetPasswordSerializer(
+            request.user,
+            data=request.data,
+            context={'request': request},
+        )
         serializer.is_valid(raise_exception=True)
         request.user.set_password(serializer.validated_data['new_password'])
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -48,13 +52,20 @@ class UserViewSet(ListCreateViewSet):
     def perform_create(self, serializer):
         """Perform actions during save an instance of user."""
         serializer.save(
-            password=make_password(self.request.user),
+            password=make_password(serializer.validated_data['password']),
         )
+
+    def get_serializer_class(self):
+        """Choose serializer class depend on method."""
+        if self.action == 'list':
+            return GetUserSerializer
+        return PostUserSerializer
 
 
 @api_view(('POST',))
+@permission_classes((AllowAny,))
 def get_token(request):
-    """An obtain token function."""
+    """Obtain token for processing."""
     serializer = GetTokenSerializer(data=request.data)
     serializer.is_valid(raise_exception=True)
 
@@ -63,7 +74,7 @@ def get_token(request):
     user = get_object_or_404(User, email=email)
 
     if not check_password(password, user.password):
-        raise serializers.ValidationError({'password': f'Пароль неверный {user.password}'})
+        raise serializers.ValidationError({'password': f'Пароль неверный'})
 
     token = RefreshToken.for_user(user)
     return Response(
